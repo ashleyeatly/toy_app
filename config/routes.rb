da50b6ad94932fa35b910a790a6a7a54a1a1003d
Ashley Eatly <ashley.eatly@bcs.org>
2022-11-07 20:02:39 +0000

    basic app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :microposts
+  # :users is a symbol
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'users#index'
 end
